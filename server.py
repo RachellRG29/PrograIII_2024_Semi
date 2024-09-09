4142522f76ab7df7073f9a4b50e003b1f4ba8831
rachellramirez29 <RG21019.ues.edu.sv>
2024-09-09 11:02:41 -0600

    cambios

--- server.py
+++ server.py
@@ -5,10 +5,11 @@ class servidorBasico(SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write("Hola mundo".encode()) 
-        
+        self.wfile.write("Bienvenidos a Progra III".encode())
+
 server = HTTPServer(('localhost', 3006), servidorBasico)
+print("Servidor ejecutado en el puerto 3006")
 server.serve_forever()
-print("Servidor ejecutado en puerto 3006")
+
         
     
