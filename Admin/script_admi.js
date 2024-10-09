c9044ace62809ab1dcf8106dffa7fea468e773f7
RachellRG29 <RachellRG29.com>
2024-10-08 20:55:56 -0600

    correcion de botones

--- Admin/script_admi.js
+++ Admin/script_admi.js
@@ -1,4 +1,4 @@
-let btn_plus = document.querySelector(".btn_plus");
+let btn_plus = document.getElementById("btn_plus");
 btn_plus.addEventListener("click", function () {
-    window.location.href = "vistaAdmin_producto.html";
+    window.location.href = "vistaAdmin_producto.html"; 
 });
