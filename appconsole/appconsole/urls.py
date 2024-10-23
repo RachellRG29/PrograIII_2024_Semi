175812778360d3c2438efa83c0d202c5a8677a0d
RachellRG29 <RachellRG29.com>
2024-10-23 12:01:04 -0600

    cambios para pasar en esta rama el proyecto de consolexpress con django, en proceso.

--- appconsole/appconsole/urls.py
+++ appconsole/appconsole/urls.py
@@ -1,24 +1,11 @@
-"""
-URL configuration for appconsole project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from consolexpress.views import index_inicio
+from consolexpress.views import index_inicio, index_login, index_register, index_pant_prin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_inicio),
+    path('', index_inicio, name='index_inicio'),
+    path('login/', index_login, name='index_login'), 
+    path('register/', index_register, name='index_register'),  
+    path('pantalla_prin/', index_pant_prin, name='index_pant_prin'),  
 ]
--- appconsole/consolexpress/views.py
+++ appconsole/consolexpress/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index_inicio(request):
     return render(request, 'index_inicio.html')
+
+def index_login(request):
+    return render(request, 'index_login.html')
+
+def index_register(request):
+    return render(request, 'index_register.html')
+
+def index_pant_prin(request):
+    return render(request, 'index_pant_prin.html')
