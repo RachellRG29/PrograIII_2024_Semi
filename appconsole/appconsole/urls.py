2569f6fc9f1a75e453f078f48dd29b19ba98e767
RachellRG29 <RachellRG29.com>
2024-10-23 22:17:37 -0600

    Agregadas todas las pantallas, htmls y css, luego veremos la implementacion de django, la logica y js.

--- appconsole/appconsole/urls.py
+++ appconsole/appconsole/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from consolexpress.views import index_inicio, index_login, index_register, index_pant_prin
+from consolexpress.views import index_inicio, index_login, index_register, index_pant_prin, crud_admi, vistaAdmin_producto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_inicio, name='index_inicio'),
     path('login/', index_login, name='index_login'), 
     path('register/', index_register, name='index_register'),  
-    path('pantalla_prin/', index_pant_prin, name='index_pant_prin'),  
+    path('pantalla_prin/', index_pant_prin, name='index_pant_prin'),
+    path('crud_admi/', crud_admi, name='crud_admi'),    
+    path('vistaAdmin_producto/', vistaAdmin_producto, name='vistaAdmin_producto'),   
 ]
--- appconsole/consolexpress/views.py
+++ appconsole/consolexpress/views.py
@@ -12,3 +12,9 @@ def index_register(request):
 
 def index_pant_prin(request):
     return render(request, 'index_pant_prin.html')
+
+def crud_admi(request):
+    return render(request, 'crud_admi.html')
+
+def vistaAdmin_producto(request):
+    return render(request, 'vistaAdmin_producto.html')
