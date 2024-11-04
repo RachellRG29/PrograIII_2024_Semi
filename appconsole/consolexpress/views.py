a0029b12f423653d0054e618bc673dedf1a4290d
jonathangolzalez <jonathangolzalez@gmail.com>
2024-11-03 21:24:14 -0600

    .

--- appconsole/consolexpress/views.py
+++ appconsole/consolexpress/views.py
@@ -18,6 +18,7 @@ def index_register(request):
 
 def index_pant_prin(request):
     return render(request, 'index_pant_prin.html')
+    
 
 def crud_admi(request):
     return render(request, 'crud_admi.html')
